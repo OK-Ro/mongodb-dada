8383b60ff880baa5d1d80efd6e78fe740491d6a5
OK-Ro <o.robert1994@hotmail.com>
2024-01-12 19:52:41 +0100

    adeed

--- Users/Orobe/OneDrive/Desktop/RolePayingGame/script.js
+++ Users/Orobe/OneDrive/Desktop/RolePayingGame/script.js
@@ -253,3 +253,11 @@ function pickTwo() {
 function pickEight() {
   pick(8);
 }
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let x = 1; x < 5; x++) {}
+}
