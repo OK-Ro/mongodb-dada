8fd1b4b42870e0c72162bc36b6e6ea9f1e7ad1dd
OK-Ro <o.robert1994@hotmail.com>
2024-02-04 18:30:14 +0100

    updates

--- Users/Orobe/OneDrive/Desktop/music-app-javascript/script.js
+++ Users/Orobe/OneDrive/Desktop/music-app-javascript/script.js
@@ -167,6 +167,8 @@ const setPlayerDisplay = () => {
   const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title");
     const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
   };
 };
 playButton.addEventListener("click", () => {
