85fd0def710d22c015e084e1bef57bc535a0ad1d
OK-Ro <o.robert1994@hotmail.com>
2024-02-13 19:34:52 +0100

    notebook

--- Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
+++ Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
@@ -9,3 +9,38 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+const taskData = [];
+let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+const title = titleInput.value.trim();
+const date = dateInput.value.trim();
+const description = descriptionInput.value.trim();
+
+const id = title.toLowerCase().split(" ").join("-");
+
+const taskObj = {
+  id: id,
+  title: title,
+  date: date,
+  description: description,
+};
