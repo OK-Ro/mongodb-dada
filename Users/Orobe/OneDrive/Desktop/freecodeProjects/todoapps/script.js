cf412b78f9dbe2a24e7d5112c5e6e92af6f92ed1
OK-Ro <o.robert1994@hotmail.com>
2024-02-13 19:54:35 +0100

    notebook

--- Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
+++ Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
@@ -34,4 +34,7 @@ const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
 const taskObj = {
   id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+  title: titleInput.value,
+  date: dateInput.value,
+  description: descriptionInput.value,
 };
