dc2a2ee51f9d2748eb33bedf4515153f6bd1a84d
OK-Ro <o.robert1994@hotmail.com>
2024-02-21 19:44:24 +0100

    updates

--- Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
+++ Users/Orobe/OneDrive/Desktop/freecodeProjects/todoapps/script.js
@@ -17,36 +17,6 @@ let currentTask = {};
 
 // Functions
 
-// Function to add or update task data
-const addOrUpdateTask = () => {
-  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-  const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
-    date: dateInput.value,
-    description: descriptionInput.value,
-  };
-
-  if (dataArrIndex === -1) {
-    taskData.unshift(taskObj);
-  }
-};
-
-// Function to update the tasks container in the DOM
-const updateTaskContainer = () => {
-  taskData.forEach(({ id, title, date, description }) => {
-    tasksContainer.innerHTML += `
-        <div class="task" id="${id}">
-          <p><strong>Title:</strong> ${title}</p>
-          <p><strong>Date:</strong> ${date}</p>
-          <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
-        </div>
-      `;
-  });
-};
-
 // Function to reset form inputs and currentTask
 const reset = () => {
   titleInput.value = "";
